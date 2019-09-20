c6c7d49a76b9b428958f99c2b4fa19686f1249b1
iota97 <giovi.cocco@gmail.com>
2019-09-21 00:12:21 +0200

    Support multiple light

--- Studies/Graphics/normal_light.py
+++ Studies/Graphics/normal_light.py
@@ -2,55 +2,72 @@
 
 # Compute light from normal + albedo
 
+# Using more step will make a rotating light series
+step = 1
+
 # Albedo image
 albedo_image = "albedo.png"
 
 # Normal image
 normal_image = "normal.png"
 
-# Light position
-position = [-10, 0, 3]
-
-# Light color
-color = [255, 255, 255]
-
-# Using more step will make a rotating light series
-step = 0
-
 # .png and frame number added automatically
 output = "out" 
 
+# Light position (Color and rotation must be AT LEAST the size of position)
+position = [[-10, 0, 3]]
+
+# Light color
+color = [[255, 255, 255], [0, 0, 128]]
+
+# Speed of rotation, 1 mean it will perform 360 degree over all the steps
+rotation_speed = [1, -1.2, 0]
+
 from PIL import Image
 import math
+import multiprocessing as mp
 
-def render(x, y, z, r, g, b, path):
-	albedo = Image.open(albedo_image)
-	normal = Image.open(normal_image)
+albedo = Image.open(albedo_image)
+normal = Image.open(normal_image)
+size = albedo.size
+light = Image.new('RGB', size, (0, 0, 0))
+a = albedo.load()
+n = normal.load()
+limg = light.load()
 
-	lc = [r/255, g/255, b/255]
+def add_light(x, y, z, r, g, b):
 	mag = math.sqrt(x*x+y*y+z*z)
 	l = [x/mag, y/mag, z/mag]
-	size = albedo.size
-	out = Image.new('RGB', size)
-	a = albedo.load()
-	n = normal.load()
-	o = out.load()
 
 	for i in range(size[0]):
 		for j in range(size[1]):
-			li = l[0]*(n[i, j][0]/127 - 1) + l[1]*(n[i, j][1]/127 - 1) + l[2]*(n[i, j][2]/127 - 1)
-			if li < 0:
-				li = 0
-			o[i, j] = (int(a[i, j][0]*li*lc[0]), int(a[i, j][1]*li*lc[1]), int(a[i, j][2]*li*lc[2]))
+			li = max(l[0]*(n[i, j][0]/127 - 1) + l[1]*(n[i, j][1]/127 - 1) + l[2]*(n[i, j][2]/127 - 1), 0)
+			limg[i, j] = (min(limg[i,j][0]+int(li*r), 255), min(limg[i,j][1]+int(li*g), 255), min(limg[i,j][2]+int(li*b), 255))
 
+def render(path):
+	out = Image.new('RGB', size)
+	o = out.load()
+	for i in range(size[0]):
+		for j in range(size[1]):
+			o[i, j] = (int(limg[i, j][0]/255*a[i, j][0]), int(limg[i, j][1]/255*a[i, j][1]), int(limg[i, j][2]/255*a[i, j][2]))
 	out.save(path)
 
-if (step == 0):
-	render(position[0], position[1], position[2], color[0], color[1], color[2], output+".png")
+def do_step(s):
+	print("Rendering frame #"+str(s+1))
+	for i in range(len(position)):
+		f = 2*float(rotation_speed[i])*math.pi/step
+		x = position[i][0]*math.cos(s*f) + position[i][1]*math.sin(s*f)
+		y = -position[i][0]*math.sin(s*f) + position[i][1]*math.cos(s*f)
+		add_light(x, y, position[i][2], color[i][0], color[i][1], color[i][2])
+	if step == 1:
+		render(output+".png")
+	else:
+		render(output+str(s)+".png")
+
+if len(position) > len(color) or len(color) > len(rotation_speed):
+	print("Error: not enough color or speed for every light!")
+
 else:
-	f = 2*math.pi/step
-	for n in range(step):
-		x = position[0]*math.sin(n*f) + position[1]*math.cos(n*f)
-		y = position[0]*math.cos(n*f) - position[1]*math.sin(n*f)
-		z = position[2]
-		render(x, y, z, color[0], color[1], color[2], output+str(n)+".png")
+	pool = mp.Pool(mp.cpu_count())
+	pool.map(do_step, range(step))
+	pool.close()
