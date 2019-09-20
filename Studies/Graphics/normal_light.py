1ed88d19544c1e05726c469a9abbaaa989b889f9
iota97 <giovi.cocco@gmail.com>
2019-09-20 14:56:45 +0200

    Normal light

--- Studies/Graphics/normal_light.py
+++ Studies/Graphics/normal_light.py
@@ -2,28 +2,49 @@
 
 #Compute light from normal + albedo
 
+# Light position
+position = [-10, 0, 3]
+
+# Light color
+color = [255, 255, 255]
+
+# Using more step will make a rotating light series
+step = 0
+
+# .png and frame number added automatically
+output = "out" 
+
 from PIL import Image
 import math
 
-light_direction = [0, 0, -10]
-light_color = [50, 50, 255]
-albedo = Image.open("albedo.png")
-normal = Image.open("normal.png")
+def render(x, y, z, r, g, b, path):
+	albedo = Image.open("albedo.png")
+	normal = Image.open("normal.png")
 
-lc = [light_color[0]/255, light_color[1]/255, light_color[2]/255]
-mag = math.sqrt(light_direction[0]*light_direction[0]+light_direction[1]*light_direction[1]+light_direction[2]*light_direction[2])
-l = [light_direction[0]/mag, light_direction[1]/mag, light_direction[2]/mag]
-size = albedo.size
-out = Image.new('RGB', size)
-a = albedo.load()
-n = normal.load()
-o = out.load()
+	lc = [r/255, g/255, b/255]
+	mag = math.sqrt(x*x+y*y+z*z)
+	l = [x/mag, y/mag, z/mag]
+	size = albedo.size
+	out = Image.new('RGB', size)
+	a = albedo.load()
+	n = normal.load()
+	o = out.load()
 
-for i in range(size[0]):
-	for j in range(size[1]):
-		li = l[0]*(n[i, j][0]/255) + l[1]*(n[i, j][1]/255) + l[2]*(n[i, j][2]/255)
-		if li < 0:
-			li +=1
-		o[i, j] = (int(a[i, j][0]*li*lc[0]), int(a[i, j][1]*li*lc[1]), int(a[i, j][2]*li*lc[2]))
+	for i in range(size[0]):
+		for j in range(size[1]):
+			li = l[0]*(n[i, j][0]/128 - 1) + l[1]*(n[i, j][1]/128 - 1) + l[2]*(n[i, j][2]/128 - 1)
+			if li < 0:
+				li = 0
+			o[i, j] = (int(a[i, j][0]*li*lc[0]), int(a[i, j][1]*li*lc[1]), int(a[i, j][2]*li*lc[2]))
 
-out.save("out.png")
+	out.save(path)
+
+if (step == 0):
+	render(position[0], position[1], position[2], color[0], color[1], color[2], output+".png")
+else:
+	f = 2*math.pi/step
+	for n in range(step):
+		x = position[0]*math.sin(n*f) + position[1]*math.cos(n*f)
+		y = position[0]*math.cos(n*f) - position[1]*math.sin(n*f)
+		z = position[2]
+		render(x, y, z, color[0], color[1], color[2], output+str(n)+".png")
