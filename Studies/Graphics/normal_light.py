9f8635b3c7d86e6a792db116d10a757c6b1b6ae7
iota97 <giovi.cocco@gmail.com>
2019-09-21 22:54:22 +0200

    Use less memory

--- Studies/Graphics/normal_light.py
+++ Studies/Graphics/normal_light.py
@@ -24,6 +24,7 @@ color = [[255, 255, 255], [0, 0, 128]]
 # Speed of rotation, 1 mean it will perform 360 degree over all the steps
 rotation_speed = [1, -1.2, 0]
 
+
 from PIL import Image
 import math
 import multiprocessing as mp
@@ -34,19 +35,17 @@ size = albedo.size
 a = albedo.load()
 n = normal.load()
 
-def add_light(x, y, z, r, g, b, s):
+def add_light(x, y, z, r, g, b, limg):
 	mag = math.sqrt(x*x+y*y+z*z)
 	l = [x/mag, y/mag, z/mag]
-	limg = ll[s]
 	for i in range(size[0]):
 		for j in range(size[1]):
 			li = max(l[0]*(n[i, j][0]/127 - 1) + l[1]*(n[i, j][1]/127 - 1) + l[2]*(n[i, j][2]/127 - 1), 0)
 			limg[i, j] = (min(limg[i,j][0]+int(li*r), 255), min(limg[i,j][1]+int(li*g), 255), min(limg[i,j][2]+int(li*b), 255))
 
-def render(path, s):
+def render(path, limg):
 	out = Image.new('RGB', size)
 	o = out.load()
-	limg = ll[s]
 	for i in range(size[0]):
 		for j in range(size[1]):
 			o[i, j] = (int(limg[i, j][0]/255*a[i, j][0]), int(limg[i, j][1]/255*a[i, j][1]), int(limg[i, j][2]/255*a[i, j][2]))
@@ -55,23 +54,21 @@ def render(path, s):
 def do_step(s):
 	print("Rendering frame #"+str(s+1))
 	
+	limg = Image.new('RGB', size, (0, 0, 0)).load()
 	for i in range(len(position)):
 		f = 2*float(rotation_speed[i])*math.pi/step
 		x = position[i][0]*math.cos(s*f) + position[i][1]*math.sin(s*f)
 		y = -position[i][0]*math.sin(s*f) + position[i][1]*math.cos(s*f)
-		add_light(x, y, position[i][2], color[i][0], color[i][1], color[i][2], s)
+		add_light(x, y, position[i][2], color[i][0], color[i][1], color[i][2], limg)
 	if step == 1:
-		render(output+".png", s)
+		render(output+".png", limg)
 	else:
-		render(output+str(s)+".png", s)
+		render(output+str(s)+".png", limg)
 
 if len(position) > len(color) or len(color) > len(rotation_speed):
 	print("Error: not enough color or speed for every light!")
 
 else:
-	ll = []
-	for i in range(step):
-		ll.append(Image.new('RGB', size, (0, 0, 0)).load())
 	pool = mp.Pool(mp.cpu_count())
 	pool.map(do_step, range(step))
 	pool.close()
