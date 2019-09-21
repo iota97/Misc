0a73148a6a2ac542134976c83dce57a3bebdf1f9
iota97 <giovi.cocco@gmail.com>
2019-09-22 00:53:17 +0200

    Cartoon from normal + albedo

---
+++ Studies/Graphics/cartoon.py
@@ -0,0 +1,74 @@
+#!/bin/python
+
+# Compute cartoon from normal + albedo
+
+# Light
+light_position = [1, 0.5, 1]
+light_color = [255, 255, 255]
+
+# Highlight
+highlight_factor = 0.5
+
+# Outline
+outline_threshold = 0.08
+outline_color = [0, 0, 0]
+
+# Posterize
+posterize_factor = 3
+
+# Albedo image
+albedo_image = "albedo.png"
+
+# Normal image
+normal_image = "normal.png"
+
+# Output image
+output = "cartoon.png" 
+
+from PIL import Image
+import math
+
+albedo = Image.open(albedo_image)
+normal = Image.open(normal_image)
+size = albedo.size
+a = albedo.load()
+n = normal.load()
+p = posterize_factor
+l = light_position
+
+lm = math.sqrt(l[0]*l[0]+l[1]*l[1]+l[2]*l[2])
+l[0] /= lm
+l[1] /= lm
+l[2] /= lm
+
+out = Image.new('RGB', size)
+o = out.load()
+for i in range(size[0]):
+		for j in range(size[1]):
+
+			i0 = max(i-1, 0)
+			i1 = min(i+1, size[0]-1)
+			j0 = max(j-1, 0)
+			j1 = min(j+1, size[1]-1)
+			f = 4*(a[i, j][0]+a[i, j][1]+a[i, j][2])
+			f -= (a[i0, j][0]+a[i0, j][1]+a[i0, j][2])
+			f -= (a[i1, j][0]+a[i1, j][1]+a[i1, j][2])
+			f -= (a[i, j0][0]+a[i, j0][1]+a[i, j0][2])
+			f -= (a[i, j1][0]+a[i, j1][1]+a[i, j1][2])
+
+			if abs(f/1024) < outline_threshold:
+				s = max(l[0]*(n[i, j][0]/127-1)+l[1]*(n[i, j][2]/127-1)+l[2]*(n[i, j][2]/127-1), 0.0)
+				if s < highlight_factor:
+					s = 1.5
+				else:
+					s = 1.0
+				
+				r = s*light_color[0]/255
+				g = s*light_color[1]/255
+				b = s*light_color[2]/255
+
+				o[i, j] = (int(round(a[i, j][0]*r/p)*p), int(round(a[i, j][1]*g/p)*p), int(round(a[i, j][2]*b/p)*p))
+			else:
+				o[i, j] = (outline_color[0], outline_color[1], outline_color[2])
+
+out.save(output)
