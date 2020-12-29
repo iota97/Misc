606ea4bc337e036dd9583819a9cb7b21280927f3
iota97 <giovi.cocco@gmail.com>
2020-12-29 22:38:40 +0100

    Clean up

--- Studies/Maths/patente.c
+++ Studies/Maths/patente.c
@@ -7,11 +7,8 @@ int main (int argc, char *argv[]) {
 		printf("Sintassi: %s <media>\n", argv[0]);
 		return 1;
 	}
-	double media = atof(argv[1]);
-	double p = 1.0;
-	for (int i = 0; i < 5; ++i) {
-		p -= pow(media, i)/(tgamma(i+1)*pow(M_E, media));
-	}
-	printf("Probabilità bocciatura: %.2f%\n", p*100);
+	double x = atof(argv[1]);
+	double p = (1+x+x*x/2+x*x*x/6+x*x*x*x/24)/pow(M_E, x);
+	printf("Probabilità promozione: %.2f%\n", p*100);
 	return 0;
 }
